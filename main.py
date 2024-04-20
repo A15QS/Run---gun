7f56e521d14f887087865534308fc21d00387efe
A15QS <friveram.qs@gmail.com>
2024-04-20 14:35:47 -0500

    all

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pygame
 import pgzrun
 import random
 import time
+from level_1 import *
+from def_map import *
 WIDTH = 1920
 HEIGHT = 1080
 TITLE = "Run_Gun"
 FPS = 60
-blocks=[]
+blocks = []
 timer_y = 925
 timer_x = 450
 for i in range(30):
@@ -258,6 +260,7 @@ game = 1
 skin = 1
 time_pj = 0
 time_hair = 0
+time_pj_menu = 0
 direction_pj = True
 move_pj = False
 speed_pj = 1
@@ -271,341 +274,12 @@ ambient = "#91ccff"
 but = False
 fps = True
 enemies = []
-my_map = [[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1, 1, 3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3,-1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,17, 2, 2,18,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-          [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]]
-my_map_noclip = [[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [-1,-1,-1,-1,-1,-1,-1, 5, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [-1,-1,-1,-1,-1,-1,-1, 5, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [ 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-                 [ 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7,-1, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-                 [ 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,20, 6, 6,21, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-                 [ 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7,-1, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]]
-my_map_to_draw = []
-def map_generator_draw_noclip():
-    for i in range(len(my_map_noclip)):
-        for j in range(len(my_map_noclip[0])):
-            if my_map_noclip[i][j] == 1:
-                new_block = Actor(blocks[0])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 2:
-                new_block = Actor(blocks[1])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 3:
-                new_block = Actor(blocks[2])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 4:
-                new_block = Actor(blocks[3])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 5:
-                new_block = Actor(blocks[4])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 6:
-                new_block = Actor(blocks[5])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 7:
-                new_block = Actor(blocks[6])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 8:
-                new_block = Actor(blocks[7])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 9:
-                new_block = Actor(blocks[8])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 10:
-                new_block = Actor(blocks[9])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 11:
-                new_block = Actor(blocks[10])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 12:
-                new_block = Actor(blocks[11])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 13:
-                new_block = Actor(blocks[12])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 14:
-                new_block = Actor(blocks[13])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 15:
-                new_block = Actor(blocks[14])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 16:
-                new_block = Actor(blocks[15])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 17:
-                new_block = Actor(blocks[16])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 18:
-                new_block = Actor(blocks[17])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 19:
-                new_block = Actor(blocks[18])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 20:
-                new_block = Actor(blocks[19])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 21:
-                new_block = Actor(blocks[20])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 22:
-                new_block = Actor(blocks[21])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 23:
-                new_block = Actor(blocks[22])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 24:
-                new_block = Actor(blocks[23])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 25:
-                new_block = Actor(blocks[24])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 26:
-                new_block = Actor(blocks[25])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 27:
-                new_block = Actor(blocks[26])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 28:
-                new_block = Actor(blocks[27])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 29:
-                new_block = Actor(blocks[28])
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-            elif my_map_noclip[i][j] == 30:
-                new_block.left = new_block.width*j
-                new_block.top = new_block.height*i
-                new_block.draw()
-def map_generator_draw():
-    for i in range(len(my_map)):
-        new_row=[]
-        for j in range(len(my_map[0])):
-            if my_map[i][j] == 1:
-                new_block = Actor(blocks[0])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 2:
-                new_block = Actor(blocks[1])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 3:
-                new_block = Actor(blocks[2])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 4:
-                new_block = Actor(blocks[3])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 5:
-                new_block = Actor(blocks[4])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 6:
-                new_block = Actor(blocks[5])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 7:
-                new_block = Actor(blocks[6])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 8:
-                new_block = Actor(blocks[7])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 9:
-                new_block = Actor(blocks[8])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 10:
-                new_block = Actor(blocks[9])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 11:
-                new_block = Actor(blocks[10])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 12:
-                new_block = Actor(blocks[11])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 13:
-                new_block = Actor(blocks[12])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 14:
-                new_block = Actor(blocks[13])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 15:
-                new_block = Actor(blocks[14])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 16:
-                new_block = Actor(blocks[15])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 17:
-                new_block = Actor(blocks[16])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 18:
-                new_block = Actor(blocks[17])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 19:
-                new_block = Actor(blocks[18])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 20:
-                new_block = Actor(blocks[19])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 21:
-                new_block = Actor(blocks[20])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 22:
-                new_block = Actor(blocks[21])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 23:
-                new_block = Actor(blocks[22])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 24:
-                new_block = Actor(blocks[23])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 25:
-                new_block = Actor(blocks[24])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 26:
-                new_block = Actor(blocks[25])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 27:
-                new_block = Actor(blocks[26])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 28:
-                new_block = Actor(blocks[27])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 29:
-                new_block = Actor(blocks[28])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-            elif my_map[i][j] == 30:
-                new_block = Actor(blocks[29])
-                new_block.left = 128*j
-                new_block.top = 128*i
-                new_row.append(new_block)
-        my_map_to_draw.append(new_row)
-map_generator_draw()
 def draw():
     global mode,mini_mode,state_click,count,click,skin_page,lang,point_s,point_c,number_1,number_2,y_number,x_number,config,counter,charge,locked_col,bon_1,bon_2,bon_3,for_player,time_pj,fps
     if time_pj == 0:
         clock.schedule_interval(animation_pj,0.3)
         clock.schedule_interval(animation_hair_pj,0.6)
-        #clock.schedule_interval(for_pj_menu,0.3)
+        clock.schedule_interval(for_pj_menu,0.3)
         clock.schedule_interval(for_charge,0.5)
         time_pj = 1
     if config == 0:
@@ -1743,7 +1417,7 @@ def update(dt):
         if mode == "game":
             pj.x += 5
             direction_pj = True
-            move_pj = True
+            move_pj = False
     if keyboard.A:
         if mode == "game":
             if pj.x >= 10:
@@ -1907,19 +1581,20 @@ def for_how():
         if game == 2:
             game -= 1
             how = 360
-#def for_pj_menu():
-    #global mode,time_pj
-    #if mode == "menu":
-        #if time_pj == 1:
-            #pj_menu.image = "Player_menu_1_move_1"
-        #elif time_pj == 2:
-            #pj_menu.image = "Player_menu_2_move_1"
-        #elif time_pj == 3:
-            #pj_menu.image = "Player_menu_3_move_1"
-        #elif time_pj == 4:
-            #pj_menu.image = "Player_menu_4_move_1"
-        #elif time_pj == 5:
-            #pj_menu.image = "Player_menu_1_move_1"
+def for_pj_menu():
+    global mode,time_pj_menu
+    time_pj_menu += 1
+    if mode == "menu":
+        if time_pj_menu == 1:
+            pj_menu.image = "Player_menu_1_move_1"
+        elif time_pj_menu == 2:
+            pj_menu.image = "Player_menu_2_move_1"
+        elif time_pj_menu == 3:
+            pj_menu.image = "Player_menu_3_move_1"
+        elif time_pj_menu == 4:
+            pj_menu.image = "Player_menu_4_move_1"
+        elif time_pj_menu == 5:
+            pj_menu.image = "Player_menu_1_move_1"
 def animation_hair_pj():
     global skin,mode,mini_mode,time_pj,direction_pj,move_pj,speed_pj,time_hair
     time_hair += 1
